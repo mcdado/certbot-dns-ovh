19ae62e0d86df4d2cc74e50ce8eb54478ccad719
David Gasperoni <mcdado@gmail.com>
2017-08-07 01:06:32 +0200

    Add first scripts: authorize and me

---
+++ bin/authorize.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const argv = require('minimist')(process.argv.slice(2));
+
+const ovh = require('ovh')({
+  endpoint: argv.endpoint || process.env.OVH_ENDPOINT,
+  appKey: argv.app_key || process.env.OVH_APP_KEY,
+  appSecret: argv.app_secret || process.env.OVH_APP_SECRET,
+});
+
+ovh.request('POST', '/auth/credential', {
+  accessRules: [
+    { method: 'GET', path: '/*' },
+    { method: 'POST', path: '/*' },
+    { method: 'PUT', path: '/*' },
+    { method: 'DELETE', path: '/*' },
+  ],
+}, (error, credential) => {
+  console.log(error || credential);
+});
