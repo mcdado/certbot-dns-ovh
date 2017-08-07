5ddee9a96564bb26d2618ec257717815203e44f9
David Gasperoni <mcdado@gmail.com>
2017-08-07 23:00:27 +0200

    Add default endpoint: ovh-eu

--- bin/authorize.js
+++ bin/authorize.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const argv = require('minimist')(process.argv.slice(2));
 
 const ovh = require('ovh')({
-  endpoint: argv.endpoint || process.env.OVH_ENDPOINT,
+  endpoint: argv.endpoint || process.env.OVH_ENDPOINT || 'ovh-eu',
   appKey: argv.app_key || process.env.OVH_APP_KEY,
   appSecret: argv.app_secret || process.env.OVH_APP_SECRET,
 });
--- bin/delete-record.js
+++ bin/delete-record.js
@@ -10,7 +10,7 @@ const certbotDomain = process.env.CERTBOT_DOMAIN;
 const dom = parseDomain(certbotDomain);
 
 const ovh = require('ovh')({
-  endpoint: argv.endpoint || process.env.OVH_ENDPOINT,
+  endpoint: argv.endpoint || process.env.OVH_ENDPOINT || 'ovh-eu',
   consumerKey: argv.key || process.env.OVH_CUSTOMER_KEY,
   appKey: argv.app_key || process.env.OVH_APP_KEY,
   appSecret: argv.app_secret || process.env.OVH_APP_SECRET,
